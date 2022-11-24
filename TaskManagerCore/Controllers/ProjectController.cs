e76346a630e867c2d12d21bc991e056351ef4a54
SivaSaiSambela <sivasai.sambela@gmail.com>
2022-11-24 11:22:29 +0530

    secondcommit

--- TaskManagerCore/Controllers/ProjectController.cs
+++ TaskManagerCore/Controllers/ProjectController.cs
@@ -33,18 +33,85 @@ namespace TaskManagerCore.Controllers
         [Route("api/projects")]
         public async Task<Project> Post([FromBody]Project project)
         {
+            //Project p= null;
             if (ModelState.IsValid)
             {
                 await _dbContext.Projects.AddAsync(project);
 
                 await _dbContext.SaveChangesAsync();
+                project = (Project)_dbContext.Projects.OrderByDescending(x => x.ProjectId).Take(1);
+                return project;
             }
             else
             {
                 return null;
             }
+            
+           
+        }
 
-            return project;
+        [HttpPut]
+        [Route("api/projects")]
+        public Project Put([FromBody]Project project)
+        {
+            Project existingProj = _dbContext.Projects.Where(x => x.ProjectId == project.ProjectId).FirstOrDefault();
+            if(existingProj!=null)
+            {
+                existingProj.ProjectName = project.ProjectName;
+                existingProj.DateOfStart = project.DateOfStart;
+                existingProj.TeamSize = project.TeamSize;
+                _dbContext.Projects.Update(existingProj);
+                _dbContext.SaveChanges();
+                return existingProj;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HttpDelete]
+        [Route("api/projects")]
+        public int Delete(int ProjectID)
+        {
+            Project existingProj = _dbContext.Projects.Where(x => x.ProjectId == ProjectID).FirstOrDefault();
+            if (existingProj != null)
+            {
+               
+                _dbContext.Projects.Remove(existingProj);
+                _dbContext.SaveChanges();
+                return ProjectID;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+
+
+        [HttpGet]
+        [Route("api/projects/search/{searchby}/{searchtext}")]
+        public List<Project> Search(string searchBy,string searchText)
+        {
+            List<Project> projects = null;
+            if (searchBy=="ProjectID")
+            {
+                 projects = (List<Project>)_dbContext.Projects.Where(x => x.ProjectId.ToString().Contains(searchText)).ToList(); ;
+            }
+            else if(searchBy=="ProjectName")
+            {
+                projects = (List<Project>)_dbContext.Projects.Where(x => x.ProjectName.Contains(searchText)).ToList();
+            }
+            if (searchBy == "DateOfStart")
+            {
+                projects = (List<Project>)_dbContext.Projects.Where(x => x.DateOfStart.ToString().Contains(searchText)).ToList();
+            }
+            if(searchBy=="TeamSize")
+            {
+                 projects = (List<Project>)_dbContext.Projects.Where(x => x.TeamSize.ToString().Contains(searchText)).ToList();
+            }
+
+            return projects;
         }
     }
 }
